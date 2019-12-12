22e06a8266916074cd0942f7800726e43a18a4d2
susu <31542614@qq.com>
2019-12-12 14:07:20 +0800

    修改source

--- tests/test_demo1.py
+++ tests/test_demo1.py
@@ -1,4 +1,13 @@
-def test_print():
-    print("test")
-def test_one():
-    print("test2")
+import requests
+
+class TestApi():
+    def test_print(self):
+        print("test")
+    def test_one(self):
+        print("test2")
+    def validate(self,key,value):
+        assert key==value
+        #循环调用
+        return self
+    def test_get(self):
+        requests.get("http://www.baidu.com").validate()
